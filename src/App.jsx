74a32cd588572fd6136ccc6fb79c3b7027da31c0
imah rahim <imahrahim@MacBook-Pro-imah.local>
2026-03-12 13:49:33 +0800

    basic portfolio layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+    <div style={{padding:"60px"}}>
+
+      <h1 style={{fontSize:"40px"}}>
+        Imah Rahim
+      </h1>
+
+      <p style={{marginTop:"10px", opacity:0.7}}>
+        Data Science · Data Visualisation · Design
       </p>
-    </>
+
+      <div style={{marginTop:"80px"}}>
+        <h2>Projects</h2>
+      </div>
+
+    </div>
   )
+
 }
 
 export default App
